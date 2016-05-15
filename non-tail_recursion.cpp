2f066fb4e564b5d085ea4d25bba17e87c7511f37
ehzShelter <emrulhasanzawad@outlook.com>
2016-05-15 08:06:24 +0600

    hey hey first push

---
+++ non-tail_recursion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+unsigned int fact(unsigned int n)
+{
+    if (n == 0) {
+        return 1;
+    }
+    return n * fact(n - 1);
+}
+
+int main(void)
+{
+    std::cout << fact(33);
+}
